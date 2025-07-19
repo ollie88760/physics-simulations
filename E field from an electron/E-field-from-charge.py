15ab4e9b8b0d1f6c0903659204976eb0d5a0f673
ollie88760 <faulkneroliver14@gmail.com>
2025-07-19 21:45:14 +0100

    asdffsad

--- E field from an electron/E-field-from-charge.py
+++ E field from an electron/E-field-from-charge.py
@@ -3,20 +3,27 @@ import numpy as np
 import scipy.constants
 import pyvista as pv
 
-epsilon_0 = 8.854187817e-12
-
 class Particle:
     def __init__(self, position, charge):
         self.position = position
         self.charge = charge
 
-origin_electron = Particle(np.array([0, 0, 0]), 1.60e-19)
+electron_1 = Particle(np.array([0, 1e-5, 0]), 1.60e-19)
+electron_2 = Particle(np.array([0, -1e-5, 0]), 1.60e-19)
 
-def calc_E_field_at_point(r, particle:Particle):
-    dist = np.linalg.norm(particle.position - r)
-    if dist == 0:
-        return np.array([0.0, 0.0, 0.0])
-    return (1/(4*np.pi*scipy.constants.epsilon_0)) * (particle.charge / (dist ** 3)) * (particle.position - r)
+my_particles = [electron_1, electron_2]
+
+def calc_E_field_at_point(r, particles:Particle):
+    field = np.array([0, 0, 0])
+    
+    for particle in particles:
+        dist = np.linalg.norm(particle.position - r)
+        if dist == 0:
+            return np.array([0.0, 0.0, 0.0])
+        
+        field = np.add(field, (1/(4*np.pi*scipy.constants.epsilon_0)) * (particle.charge / (dist ** 3)) * (particle.position - r))
+
+    return field
 
 grid_size = 31
 spacing = 1e-6
@@ -24,14 +31,12 @@ spacing = 1e-6
 x = np.linspace(-spacing*grid_size/2, spacing*grid_size/2, grid_size)
 y = np.linspace(-spacing*grid_size/2, spacing*grid_size/2, grid_size)
 z = np.linspace(-spacing*grid_size/2, spacing*grid_size/2, grid_size)
-print(list(x))
-
 
 
 X, Y, Z = np.meshgrid(x, y, z, indexing="ij")
 points = np.column_stack([X.flatten(), Y.flatten(), Z.flatten()])
 
-E_vectors = np.array([calc_E_field_at_point(p, origin_electron) for p in points])
+E_vectors = np.array([calc_E_field_at_point(p, my_particles) for p in points])
 magnitude = np.linalg.norm(E_vectors, axis=1)
 magnitude[magnitude==0] = 1e-12
 logE = np.log(magnitude)
